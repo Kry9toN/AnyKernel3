257027e333ac46b27ee425f68031c87983f3f67b
itsmeley <itsmeley@nndrive.my.id>
2021-03-28 13:20:04 +0700

    module: update
    value taken from ktweak by tytydraco

--- perf/post-fs-data.sh
+++ perf/post-fs-data.sh
@@ -1,5 +1,14 @@
-#VM
-echo 40 > /proc/sys/vm/dirty_ratio
-echo 60 > /proc/sys/vm/vfs_cache_pressure
-echo 85 > /proc/sys/vm/swappiness
-echo 3600 > /proc/sys/vm/stat_interval
+# VM
+echo 30 > /proc/sys/vm/dirty_ratio
+echo 100 > /proc/sys/vm/vfs_cache_pressure
+echo 100 > /proc/sys/vm/swappiness
+echo 10 > /proc/sys/vm/stat_interval
+echo 10 > /proc/sys/vm/dirty_background_ratio
+echo 3000 > /proc/sys/vm/dirty_expire_centisecs
+echo 3000 > /proc/sys/vm/dirty_writeback_centisecs
+echo 0 > /proc/sys/vm/page-cluster
+
+# TCP
+echo 1 > /proc/sys/net/ipv4/tcp_ecn
+echo 3 > /proc/sys/net/ipv4/tcp_fastopen
+echo 0 > /proc/sys/net/ipv4/tcp_syncookies
