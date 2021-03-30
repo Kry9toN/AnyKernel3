1d6f450bb0323b69225f86e8476e045554f847a6
itsmeley <itsmeley@nndrive.my.id>
2021-03-30 22:59:27 +0700

    module: update
    taken from ktweak by tytydraco

--- perf/post-fs-data.sh
+++ perf/post-fs-data.sh
@@ -1,9 +1,9 @@
 # VM
 echo 30 > /proc/sys/vm/dirty_ratio
-echo 100 > /proc/sys/vm/vfs_cache_pressure
+echo 80 > /proc/sys/vm/vfs_cache_pressure
 echo 100 > /proc/sys/vm/swappiness
 echo 10 > /proc/sys/vm/stat_interval
-echo 10 > /proc/sys/vm/dirty_background_ratio
+echo 15 > /proc/sys/vm/dirty_background_ratio
 echo 3000 > /proc/sys/vm/dirty_expire_centisecs
 echo 3000 > /proc/sys/vm/dirty_writeback_centisecs
 echo 0 > /proc/sys/vm/page-cluster
@@ -12,3 +12,12 @@ echo 0 > /proc/sys/vm/page-cluster
 echo 1 > /proc/sys/net/ipv4/tcp_ecn
 echo 3 > /proc/sys/net/ipv4/tcp_fastopen
 echo 0 > /proc/sys/net/ipv4/tcp_syncookies
+
+#SCHEDULER
+write /proc/sys/kernel/sched_schedstats 0
+write /proc/sys/kernel/sched_tunable_scaling 0
+write /proc/sys/kernel/sched_child_runs_first 0
+write /proc/sys/kernel/sched_autogroup_enabled 0
+write /proc/sys/kernel/sched_migration_cost_ns 5000000
+write /proc/sys/kernel/perf_cpu_time_max_percent 20
+write /proc/sys/kernel/sched_nr_migrate 128
